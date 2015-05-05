9cb73a5d57f2aa3f03e3dc8296a626c43987ab87
Anirudh Sanjeev <skyronic@gmail.com>
2015-05-05 18:48:03 +0530

    continuing on sql provider clean rewrite

--- src/CrudKit/Data/SQL/SQLColumn.php
+++ src/CrudKit/Data/SQL/SQLColumn.php
@@ -8,7 +8,27 @@ abstract class SQLColumn {
     public $category = null;
     public $options = null;
 
-    public function __construct ($id, $category, $options) {
+    const CATEGORY_VALUE = "value";
+    const CATEGORY_PRIMARY = "primary";
+    const CATEGORY_FOREIGN = "foreign";
 
+    public function __construct ($id, $category, $options) {
+        $this->id = $id;
+        $this->category = $category;
+        $this->options = $options;
+    }
+
+    public function doctrineColumnLookup ($col_lookup) {
+        // Usually a child will override this
+    }
+
+    public function init () {
+        // A child will override this
+    }
+
+    public function getExpr () {
+        throw new \Exception("This column doesn't support expressions. this shouldn't be caled");
+        /** @noinspection PhpUnreachableStatementInspection */
+        return "";
     }
 }
--- src/CrudKit/Data/SQLiteDataProvider.php
+++ src/CrudKit/Data/SQLiteDataProvider.php
@@ -17,6 +17,31 @@ use PDO;
 use utilphp\util;
 
 class SQLiteDataProvider extends BaseSQLDataProvider{
+
+    public function addColumn ($expr, $label, $options = array()) {
+        $options['label'] = $label;
+        $options['expr'] = $expr;
+
+        $this->internalAddColumn(SQLColumn::CATEGORY_VALUE, $expr, $options);
+    }
+
+    public function setPrimaryColumn ($expr) {
+        $this->internalAddColumn(SQLColumn::CATEGORY_PRIMARY, $expr, array(
+            'name' => "Primary",
+            'expr' => $expr
+        ));
+    }
+
+    public function manyToOne ($foreignKey, $extTable, $primary, $nameColumn, $label) {
+        $this->internalAddColumn(SQLColumn::CATEGORY_FOREIGN, $foreignKey, array(
+            'fr_type' => 'manyToOne',
+            'fr_table' => $extTable,
+            'fk_primary' => $primary,
+            'fk_name_col' => $nameColumn,
+            'label' => $label
+        ));
+    }
+
     public function init () {
         parent::init();
 
@@ -28,7 +53,15 @@ class SQLiteDataProvider extends BaseSQLDataProvider{
     }
 
     /**
-     *
+     * @param array $summary_cols
+     */
+    public function setSummaryColumns($summary_cols)
+    {
+        $this->summary_cols = $summary_cols;
+    }
+
+    /**
+     * Converts columns from raw objects to more powerful cool objects
      */
     protected function processColumns () {
         // Get a schema manager and get the list of columns
@@ -68,8 +101,64 @@ class SQLiteDataProvider extends BaseSQLDataProvider{
                     throw new \Exception("Unknown category for column $category");
             }
 
+            $target->doctrineColumnLookup($type_lookup);
+            $target->init ();
 
+            $this->columns[$id] = $target;
         }
+    }
+
+    /**
+     * Super cool and useful function to query columns and get a reduced
+     * subset
+     *
+     * @param $queryType
+     * @param $queryValues
+     * @param $valueType
+     * @return array
+     */
+    protected function queryColumns ($queryType, $queryValues, $valueType) {
+        $target_columns = array();
+
+        if($queryType === "col_list") {
+            // The caller has already specified a list of columns
+            $target_columns = $queryValues;
+        }
+        else {
+            /**
+             * @var  $key
+             * @var SQLColumn $col
+             */
+            foreach($this->columns as $key => $col) {
+                if($queryType === "category") {
+                    if(in_array($col->category, $queryValues)) {
+                        $target_columns []= $key;
+                    }
+                }
+            }
+        }
+
+        $results = array();
+
+        foreach($target_columns as $colKey) {
+            /**
+             * @var $column SQLColumn
+             */
+            $column = $this->columns[$colKey];
+            switch($valueType) {
+                case "id":
+                    $results []= $colKey;
+                    break;
+                case "expr":
+                    $results []= $column->getExpr();
+                    break;
+                case "object":
+                    $results []= $column;
+                    break;
+            }
+        }
+
+        return $results;
     }
 
     protected function internalAddColumn ($id, $category, $options = array()) {
@@ -81,32 +170,8 @@ class SQLiteDataProvider extends BaseSQLDataProvider{
     }
 
     /**
-     * Column definitions which are raw arrays and havne't been cast into
-     * the appropriate SQLColumn
-     *
-     * @var array
+     * @param $table string
      */
-    protected $colDefs = array();
-
-    /**
-     * An array of SQL Columns
-     *
-     * @var array[SQLColumn]
-     */
-    protected $columns = array();
-
-
-    /** @var Connection */
-    protected $conn = null;
-
-    /**
-     * The path of the sqlite file to open.
-     * @var string
-     */
-    protected $path = null;
-
-    protected $tableName = null;
-
     public function setTable ($table) {
         $this->tableName = $table;
     }
@@ -150,4 +215,43 @@ class SQLiteDataProvider extends BaseSQLDataProvider{
     {
         // TODO: Implement getEditFormConfig() method.
     }
+
+    /**
+     * Column definitions which are raw arrays and havne't been cast into
+     * the appropriate SQLColumn
+     *
+     * @var array
+     */
+    protected $colDefs = array();
+
+    /**
+     * An array of SQL Columns
+     *
+     * @var array[SQLColumn]
+     */
+    protected $columns = array();
+
+
+    /** @var Connection */
+    protected $conn = null;
+
+    /**
+     * The path of the sqlite file to open.
+     * @var string
+     */
+    protected $path = null;
+
+    /**
+     * Name of the table
+     *
+     * @var string
+     */
+    protected $tableName = null;
+
+    /**
+     * A list of summary columns
+     *
+     * @var array
+     */
+    protected $summary_cols = array();
 }
