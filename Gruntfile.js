04877cf2c0d4b88d29711dd075a2651acbdf8614
Anirudh Sanjeev <skyronic@gmail.com>
2015-05-15 18:01:08 +0530

    concat all minified files for much smaller size

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+  var vendorRoot = "src/static/vendor/";
+  var buildRoot = "src/static/build/";
   grunt.initConfig({
     watch: {
         lessFiles: {
@@ -8,7 +10,13 @@ module.exports = function(grunt) {
         }
     },
     copy:{
-
+      libs: {
+        files: [
+        {expand: true, cwd: vendorRoot + 'bootstrap/dist/', src:["fonts/*"], dest: buildRoot},
+        {expand: true, cwd: vendorRoot + 'adminlte/dist/', src:["img/*"], dest: buildRoot},
+        {expand: true, cwd: vendorRoot + 'fontawesome/', src:["fonts/*"], dest: buildRoot},
+        ]
+      }
     },
     less: {
       main: {
@@ -18,7 +26,38 @@ module.exports = function(grunt) {
       }
     },
     concat: {
-
+      css: {
+        files: {
+          "src/static/build/css/crudkit-libs.min.css":[
+            vendorRoot + "bootstrap/dist/css/bootstrap.min.css",
+            vendorRoot + "angular-busy/dist/angular-busy.min.css",
+            vendorRoot + "adminlte/dist/css/AdminLTE.min.css",
+            vendorRoot + "adminlte/dist/css/skins/skin-blue.css",
+            vendorRoot + "fontawesome/css/font-awesome.min.css",
+            vendorRoot + "bootstrap3-dialog/dist/css/bootstrap-dialog.min.css"
+          ]
+        }
+      },
+      js: {
+        options: {
+          separator: ";\n"
+        },
+        files: {
+          "src/static/build/js/crudkit-libs.min.js": [
+          vendorRoot + "jquery/dist/jquery.min.js",
+          vendorRoot + "bootstrap/dist/js/bootstrap.min.js",
+          vendorRoot + "lodash/lodash.min.js",
+          vendorRoot + "angularjs/angular.min.js",
+          vendorRoot + "angular-animate/angular-animate.min.js",
+          vendorRoot + "angular-busy/dist/angular-busy.min.js",
+          vendorRoot + "angular-filter/dist/angular-filter.min.js",
+          vendorRoot + "bootstrap3-dialog/dist/js/bootstrap-dialog.min.js",
+          vendorRoot + "jsurl/url.min.js",
+          vendorRoot + "vendor/moment/min/moment.min.js",
+          vendorRoot + "vendor/adminlte/dist/js/app.min.js",
+          ]
+        }
+      }
     },
     clean: {
 
