f30a21a77691a03ac1f2c6584eaedd513c02000f
Sean Mumford <sean.mumford@gmail.com>
2016-03-30 08:51:55 -0500

    updating code formatting to match PSR-2

--- src/CrudKit/Pages/MySQLTablePage.php
+++ src/CrudKit/Pages/MySQLTablePage.php
@@ -1,32 +1,29 @@
 <?php
-
 namespace CrudKit\Pages;
 
-
-use CrudKit\Pages\BaseSQLDataPage;
 use Doctrine\DBAL\DriverManager;
 
-class MySQLTablePage extends BaseSQLDataPage {
-    public function __construct ($id, $user, $pass, $db, $extra = array()) {
-        $params = array(
-            'driver' => 'pdo_mysql',
-            'user' => $user,
+class MySQLTablePage extends BaseSQLDataPage
+{
+    public function __construct($id, $user, $pass, $db, $extra = [])
+    {
+        $params = [
+            'driver'   => 'pdo_mysql',
+            'user'     => $user,
             'password' => $pass,
-            'dbname' => $db
-        );
+            'dbname'   => $db,
+        ];
 
-        if(isset($extra['host'])) {
+        if (isset($extra['host'])) {
             $params['host'] = $extra['host'];
         }
-        if(isset($extra['port'])) {
+        if (isset($extra['port'])) {
             $params['port'] = $extra['port'];
         }
-        if(isset($extra['charset'])) {
+        if (isset($extra['charset'])) {
             $params['charset'] = $extra['charset'];
         }
         $conn = DriverManager::getConnection($params);
         $this->preInit($id, $conn);
-
-        return $this;
     }
 }
